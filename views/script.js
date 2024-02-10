394510cb41a1f02c9eae32f99b183478ca467c3c
Jazer <saltanhayrliev2006@gmail.com>
2024-02-10 16:28:23 +0300

    update with numbers inputs

--- views/script.js
+++ views/script.js
@@ -1,6 +1,24 @@
 const btn = document.getElementById('btn')
 const answer = document.querySelector('.noAnswer')
-
-btn.addEventListener('click',(e) => {
-	answer.className = "Answer";
-})
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+const three = document.querySelector('.three')
+const four = document.querySelector('.four')
+const five = document.querySelector('.five')
+const six = document.querySelector('.six')
+const seven = document.querySelector('.seven')
+const eight = document.querySelector('.eight')
+const nine = document.querySelector('.nine')
+const zero = document.querySelector('.zero')
+const arr = [zero,one,two,three,four,five,six,seven,eight,nine]
+for(let i = 0; i< arr.length;i++){
+	arr[i].addEventListener('click', (e) => {
+		if(answer.textContent == 'Please input values'){
+			answer.textContent=String(i);
+			answer.className = 'Answer'
+		}
+		else{
+			answer.textContent += String(i);
+		}
+	})
+}

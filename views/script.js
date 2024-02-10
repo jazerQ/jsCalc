cb1e566938b3e19b9c83f0ef604ef4c0c1406dd5
Jazer <saltanhayrliev2006@gmail.com>
2024-02-10 19:32:31 +0300

    first solid step

--- views/script.js
+++ views/script.js
@@ -13,35 +13,19 @@ const zero = document.querySelector('.zero');
 const doubleZero = document.querySelector('.doubleZero');
 const deleteOne = document.querySelector('.deleteOne');
 const deleteAll = document.querySelector('.deleteAll');
+import { Arifmetic, Delete } from './classes.js'
+const arifm = new Arifmetic;
+const deleting = new Delete;
 const arr = [zero, one, two, three, four, five, six, seven, eight, nine];
 
-deleteOne.addEventListener('click', (e) => {
-	if(answer.textContent !== 'Please input values'){
-		let calcWord = answer.textContent;
-		calcWord = calcWord.split('')
-		calcWord.splice(calcWord.length-1,1)
-		answer.textContent = calcWord.join('')
-		if(answer.textContent == ''){
-			answer.textContent = '0'
-		}
-	}
-})
+deleteOne.addEventListener('click', deleting.deleteOne)
 
-deleteAll.addEventListener('click', () => {
-	if(answer.textContent !== 'Please input values'){
-		answer.textContent = '0';
-	}
-})
+deleteAll.addEventListener('click', deleting.deleteAll)
 
 for(let i = 0; i< arr.length;i++){
 	arr[i].addEventListener('click', (e) => {
-		if(answer.textContent == 'Please input values'){
-			answer.textContent=String(i);
-			answer.className = 'Answer'
-		}
-		else{
-			answer.textContent += String(i);
-		}
+		answer.textContent = arifm.write(i, answer.textContent);
+		answer.className = "Answer";
 	})
 }
 doubleZero.addEventListener('click',() => {
